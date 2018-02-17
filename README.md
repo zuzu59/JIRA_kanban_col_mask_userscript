25c2ca059f0af02d74b41ddcfff11cf8395a2c2a
Christian Zufferey <christian@zufferey.com>
2018-02-17 11:47:55 +0100

    wip

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # JIRA_kanban_col_mask_userscript
 Petit usercript tout simple pour masquer certaines colonnes d'un tableau Kanban
+
+## Installation
+Installer http://tampermonkey.net/ sur son browser
+
+Simplement cliquer sur:
+
+### Pour la version Simplex
+xxx
+
+
+### Pour la version Complex
+xxx
+
+
+
--- col_mask_simplex.user.js
+++ col_mask_simplex.user.js
@@ -1,15 +1,14 @@
 // ==UserScript==
-// @name        EPFL People
-// @namespace   none
-// @description A script to improve browsing on people.epfl.ch
-// @include     https://people.epfl.ch/*
-// @include     https://personnes.epfl.ch/*
-// @version     1.3
-// @grant       GM_xmlhttpRequest
-// @grant       GM_addStyle
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
-// @author      EPFL-dojo
-// @downloadURL https://raw.githubusercontent.com/epfl-dojo/EPFL_People_UserScript/master/EPFL_People.user.js
+// @name         JIRA Kanban col mask Simplex
+// @namespace    none
+// @description  Masque pas les colonnes Backlog et Done. Et agrandi la colonne détails
+// @version      zf180217.1106
+// @author       Ch. Zufferey
+// @include      https://siexop-jirat.epfl.ch/secure/RapidBoard.jspa*
+// @match        none
+// @grant        none
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @downloadURL  https://github.com/zuzu59/JIRA_kanban_col_mask_userscript/raw/master/col_mask_simplex.user.js
 // ==/UserScript==
 
 this.$ = this.jQuery = jQuery.noConflict(true);
@@ -25,3 +24,5 @@ setInterval(function(){
 }, 3000);
 console.log("toto");
 })();
+
+
